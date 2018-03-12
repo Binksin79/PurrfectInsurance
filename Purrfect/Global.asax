a234ebd1739093c612a025285e60550a031f41ab
Rob Roy <r.m.royiii@gmail.com>
2018-03-12 09:59:45 -0400

    Add project files.

---
+++ Purrfect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Purrfect.MvcApplication" Language="C#" %>
---
+++ Purrfect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Purrfect.Startup))]
+namespace Purrfect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
